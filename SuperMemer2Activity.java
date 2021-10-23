e3c03076d15b3dbe0a339f2c457aa500d9bd6677
MissJ32 <71284691+MissJ32@users.noreply.github.com>
2021-10-23 18:04:37 -0400

    Add files via upload

---
+++ SuperMemer2Activity.java
@@ -0,0 +1,14 @@
+package com.example.supermemer2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuperMemer2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_memer_2);
+    }
+}
